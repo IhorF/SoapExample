05aa3e7b237b30adcf5c0a3e4594b0345958debb
Malynovsky-Ivan <km.lab6@gmail.com>
2018-02-02 18:10:44 +0200

    Another commit

--- src/main/java/com/soapexample/endpoint/DocumentEndpoint.java
+++ src/main/java/com/soapexample/endpoint/DocumentEndpoint.java
@@ -1,17 +1,19 @@
 package com.soapexample.endpoint;
 
-import com.soapexample.generated.Document;
 import com.soapexample.generated.StoreDocumentRequest;
 import com.soapexample.generated.StoreDocumentResponse;
 import com.soapexample.somelogic.WordService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.context.MessageContext;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.saaj.SaajSoapMessage;
 
+import javax.activation.DataHandler;
 import java.io.*;
 import java.math.BigInteger;
 
@@ -26,13 +28,14 @@ public class DocumentEndpoint {
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "storeDocumentRequest")
     @ResponsePayload
-    public StoreDocumentResponse storeDocument(@RequestPayload StoreDocumentRequest request)
+    public StoreDocumentResponse storeDocument(@RequestPayload StoreDocumentRequest request, MessageContext context)
 			throws IOException {
 
-        Document document = request.getDocument();
         StoreDocumentResponse response = new StoreDocumentResponse();
-        response.setCount(BigInteger.valueOf(wordService.getOccurrenceInStream(document.getWord(),
-				document.getContent().getInputStream())));
+		SaajSoapMessage message = (SaajSoapMessage) context.getRequest();
+		DataHandler handler = message.getAttachment("content").getDataHandler();
+        response.setCount(BigInteger.valueOf(wordService.getOccurrenceInStream(request.getWord(),
+				request.getContent().getInputStream())));
 
         return response;
     }
